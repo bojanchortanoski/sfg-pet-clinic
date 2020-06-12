ac2495e299bf827c60da93d36639ee089e0e84b7
Bojan Chortanoski <bojan.chortanoski@musala.com>
2020-06-12 21:43:55 +0200

    Added owners list. Closes #16

--- pet-clinic-web/src/main/java/com/timeout/gambit/sfgpetclinic/controllers/PetOwnerController.java
+++ pet-clinic-web/src/main/java/com/timeout/gambit/sfgpetclinic/controllers/PetOwnerController.java
@@ -1,14 +1,22 @@
 package com.timeout.gambit.sfgpetclinic.controllers;
 
+import com.timeout.gambit.sfgpetclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping({"/owner"})
 public class PetOwnerController {
+    private final OwnerService ownerService;
+
+    public PetOwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
 
     @RequestMapping({"", "/", "/index", "/index.html"})
-    public String listOwners() {
+    public String listOwners(Model model) {
+        model.addAttribute("owners", ownerService.findAll());
         return "/owners/index";
     }
 }
